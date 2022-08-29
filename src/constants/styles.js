dd9792b4c3f2be941e810a3e5fac25292b3b1727
Phanindra Duvvuri <phanindra40430@gmail.com>
2022-08-29 23:10:25 +0530

    Add css-reset and basic styles.

---
+++ src/constants/styles.js
@@ -0,0 +1,11 @@
+// PRIMARY COLORS
+export const DESATURATED_DARK_CYAN = "hsl(180, 29%, 50%)";
+// NEUTRAL COLORS
+export const BODY_BG = "hsl(180, 52%, 96%)";
+export const FILTER_TABLET_BG = "hsl(180, 31%, 95%)";
+export const MAIN_FG = "hsl(180, 8%, 52%)";
+export const JOB_FG = "hsl(180, 14%, 20%)";
+
+export const FONT_SIZE = "1.5rem";
+export const FONT_WEIGHT_NORMAL = 500;
+export const FONT_WEIGHT_BOLD = 700;
